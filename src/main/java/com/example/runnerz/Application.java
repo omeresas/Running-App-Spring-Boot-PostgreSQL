15395b8b14d26f7f7c175de77d14810d122ef47f
omer esas <omeresas@192.168.1.15>
2024-11-06 23:32:27 +0300

    Initial commit

---
+++ src/main/java/com/example/runnerz/Application.java
@@ -0,0 +1,30 @@
+package com.example.runnerz;
+
+import com.example.runnerz.run.Location;
+import com.example.runnerz.run.Run;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+@SpringBootApplication
+public class Application {
+//	private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+//	@Bean
+//	CommandLineRunner runner() {
+//		return args -> {
+//			Run run = new Run(1, "First run", LocalDateTime.now(), LocalDateTime.now().plus(1, ChronoUnit.HOURS), 3, Location.OUTDOOR);
+//			log.info("Run " + run);
+//		};
+//	}
+}
---
+++ src/main/java/com/example/runnerz/run/RunRepository.java
@@ -0,0 +1,55 @@
+package com.example.runnerz.run;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class RunRepository {
+    private List<Run> runs = new ArrayList<>();
+
+    List<Run> findAll() {
+        return runs;
+    }
+
+    Optional<Run> findById(Integer id) {
+        return runs.stream()
+                .filter(run -> run.id().equals(id))
+                .findFirst();
+    }
+
+    void create(Run run) {
+        runs.add(run);
+    }
+
+    void update(Run run, Integer id) {
+        Optional<Run> existingRun = findById(id);
+        existingRun.ifPresent(value -> runs.set(runs.indexOf(value), run));
+    }
+
+    void delete(Integer id) {
+        runs.removeIf(value -> value.id().equals(id));
+    }
+
+    @PostConstruct
+    private void init() {
+        runs.add(new Run(
+                1,
+                "Monday Morning Run",
+                LocalDateTime.now(),
+                LocalDateTime.now().plusMinutes(30),
+                3,
+                Location.INDOOR));
+        runs.add(new Run(
+                2,
+                "Wednesday Evening Run",
+                LocalDateTime.now(),
+                LocalDateTime.now().plusMinutes(60),
+                6,
+                Location.INDOOR));
+    }
+}
