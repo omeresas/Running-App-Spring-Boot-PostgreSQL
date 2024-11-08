0821f5cab0f2d8752dcaa3f733f70145c9c61d37
omeresas <omer.esas@gmail.com>
2024-11-08 22:41:25 +0300

    Add UserHttpClient

--- src/main/java/com/example/runnerz/Application.java
+++ src/main/java/com/example/runnerz/Application.java
@@ -1,11 +1,14 @@
 package com.example.runnerz;
 
 import com.example.runnerz.user.User;
-import com.example.runnerz.user.UserRestClient;
+import com.example.runnerz.user.UserHttpClient;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 import java.util.List;
 
@@ -16,7 +19,14 @@ public class Application {
 	}
 
 	@Bean
-	CommandLineRunner runner(UserRestClient client) {
+	UserHttpClient userHttpClient() {
+		RestClient restClient = RestClient.create("https://jsonplaceholder.typicode.com/");
+		HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient)).build();
+		return factory.createClient(UserHttpClient.class);
+	}
+
+	@Bean
+	CommandLineRunner runner(UserHttpClient client) {
 		return args -> {
 			List<User> users = client.findAll();
 			System.out.println(users);
